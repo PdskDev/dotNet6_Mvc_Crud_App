cbaa29f2cd6b7c8868be7e84ccc9d127c3244185
PdskDev <prestadesk@gmail.com>
2023-08-07 19:49:28 +0200

    Ajoutez des fichiers projet.

---
+++ dotNet6_Mvc_Crud_App/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotNet6_Mvc_Crud_App.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ dotNet6_Mvc_Crud_App/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using dotNet6_Mvc_Crud_App.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNet6_Mvc_Crud_App.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
